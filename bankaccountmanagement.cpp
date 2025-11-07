681ad55284197d51cc800f000066e488bc6fe222
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2025-11-07 20:53:14 +0530

    Update bankaccountmanagement.cpp

--- bankaccountmanagement.cpp
+++ bankaccountmanagement.cpp
@@ -1,3 +1,62 @@
+                                       ----4.BANK ACCOUNT MANAGEMENT----
+
+🧭 Aim
+
+To write a C++ program that defines a class BankAccount to maintain customer account details using constructors, member functions, and a static data member to count the total number of accounts.
+
+🧭 Algorithm
+	
+STEP 1:Start the program.
+
+STEP 2:Include the header file #include <iostream> to allow input and output operations.
+
+STEP3:Use the std namespace.
+
+STEP 4:Define a class BankAccount containing:
+Data members:
+
+int accNo — to store account number.
+
+string name — to store the account holder’s name.
+
+float balance — to store the current balance.
+
+static int count — to keep track of the total number of accounts created.
+
+Constructors:
+
+Default Constructor — initializes all data members to zero or empty values and increments count.
+
+Parameterized Constructor — initializes data members with user-provided values and increments count.
+
+Member functions:
+
+deposit(float amt) — to add an amount to the balance.
+
+withdraw(float amt) — to deduct an amount from the balance.
+
+display() — to display account details.
+
+static showCount() — to display the total number of accounts created.
+
+STEP 5:Define and initialize the static data member count to 0 outside the class.
+
+STEP 6:In the main() function:
+
+Create objects using default and parameterized constructors.
+
+Perform deposit and withdrawal operations.
+
+Display each account’s details using the display() function.
+
+Call the static function showCount() to display the total number of accounts created.
+
+STEP 7:End the program.
+
+
+
+🧭 Code
+
 #include <iostream>
 using namespace std;
 class BankAccount {
@@ -32,3 +91,6 @@ b3.display();
 BankAccount::showCount();
 return 0;
 }
+
+
+
