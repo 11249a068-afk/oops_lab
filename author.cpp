b837640888e81509b925adcdea0dc6204f12f4c9
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2025-11-07 19:54:31 +0530

    Update author.cpp

--- author.cpp
+++ author.cpp
@@ -1,3 +1,42 @@
+                                            2. AUTHOR OF BOOKS 
+                                                                                                                                   
+🧭 Aim
+
+To write a C++ program that defines a class book with data members title, author, and price, and to display the details of books using a member function.
+
+🧭 Algorithm
+	
+STEP 1:Start the program.
+
+STEP 2:Include the header file #include <iostream> to use input/output functions.
+
+STEP 3:Use the std namespace so that we can directly use cout, string, etc.
+
+STEP 4:Define a class book containing:
+
+Data members:
+
+string title — to store the book title.
+
+string author — to store the author’s name.
+
+float price — to store the price of the book.
+
+Member function:
+
+void display() — to display the details of a book (title, author, price).
+
+STEP 5:In the main() function:
+
+Create two objects of class book: b1 and b2.
+
+Assign values to each book’s data members (title, author, price).
+
+Call the display() function for both objects to print their details.
+
+STEP 6:End the program.
+
+🧭 Code
 #include<iostream>
 using namespace std;
 class book
@@ -25,3 +64,8 @@ int main(){
     return 0;
 
 }
+
+
+
+
+
