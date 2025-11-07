f4f89add0d31e7bd820bb81b1186f6a91d741c2a
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2025-11-07 21:25:03 +0530

    Update employeesalarysystem.cpp

--- employeesalarysystem.cpp
+++ employeesalarysystem.cpp
@@ -1,3 +1,37 @@
+                                          ----8.EMPLOYEES SALARY SYSTEM----                                                                                                                              
+
+🧭 Aim
+
+To write a C++ program that defines a class Employee with data members such as id, name, and salary, and a member function to calculate and display a 10% bonus for each employee using constructors (including a default constructor).
+
+🧭 Algorithm
+
+1.Start the program.
+
+2.Define a class Employee with private data members: id, name, salary.
+
+3.Create a constructor with default arguments (i=0, n="Unknown", s=0.0) to initialize data members.
+
+4,Define a member function calcBonus() that:
+
+Calculates bonus as 0.1 * salary.
+
+Prints the employee's ID, name, salary, and bonus.
+
+5.In the main() function:
+
+Create objects of Employee:
+
+e1 and e2 using parameterized constructor.
+
+e3 using default constructor.
+
+Call calcBonus() for each employee.
+
+6.End the program.
+
+🧭 Code
+
 #include <iostream>
 using namespace std;
 class Employee {
