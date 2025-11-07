1e606b49fb433554669d2b5a423e044a9c1e62be
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2025-11-07 21:09:53 +0530

    Update companyLaptop.cpp

--- companyLaptop.cpp
+++ companyLaptop.cpp
@@ -1,3 +1,19 @@
+🧭 Aim
+
+To write a C++ program that defines a class car with data members model, company, and price, and displays the details of cars using a member function.
+
+🧭 Algorithm
+
+	
+1. Start the program.
+2. Define a class Laptop with data members brand, processor, and ram.
+3. Define the member function display() to print laptop details.
+4. In main(), create two objects l1 and l2.
+5. Assign values to the data members.
+6. Call display() for each object.
+7. Stop the program.
+
+🧭 Code
 #include<iostream>
 using namespace std;
 class laptop
@@ -21,4 +37,5 @@ int main(){
     l1.display();
     l2.display();
     return 0;
-        }
+}
+
