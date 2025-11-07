c3b655105849099fe5ed3bbc121b091008287292
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2025-11-07 19:44:14 +0530

    Update Recordofstudents.cpp

--- Recordofstudents.cpp
+++ Recordofstudents.cpp
@@ -1,3 +1,46 @@
+  
+                                                              1.RECORDS OF STUDENTS   
+
+🧭 Ai
+
+To write a C++ program that defines a class student having data members name, rollno, and marks, and to display the details of multiple students using a member function.
+
+🧭 Algorithm
+
+STEP 1
+
+STEP 1:Start the program.
+
+STEP 2:Include the required header file #include <iostream>.
+
+STEP 3:Use the std namespace so that standard C++ objects (like cout, string) can be used without the std:: prefix.
+
+Define a class named student with the following members:
+
+Data members:
+
+STEP 4: string name — to store the student’s name.
+
+int rollno — to store the student’s roll number.
+
+float marks — to store the student’s marks.
+
+Member function:
+
+void display() — to print the student’s details.
+
+STEP 5:In the main() function:
+
+Create three objects of the class: s1, s2, and s3.
+
+Assign values to their data members (name, rollno, marks).
+
+Call the display() function for each object to print the details.
+
+STEP 6:End the program.
+
+🧭 Code
+
 #include<iostream.h>
 using namespace std;
 class student
@@ -33,3 +76,4 @@ return 0;
 
 
 }
+
