df3e99ebc8c0d88e61b07277e7b0070bcaf3d707
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2025-11-09 19:08:03 +0530

    Create U-4Overriding Member Functions.cpp

---
+++ U-4Overriding Member Functions.cpp
@@ -0,0 +1,41 @@
+                                              --- Overriding Member Functions---
+
+🧭 Aim
+
+//To show function overriding using inheritance.//
+
+🧭 Algorithm
+
+
+1. Create base Account class with displayBalance().
+2. Derive Savings and Current overriding the function.
+3. Use base pointer to call them.
+
+🧭 Code
+#include <iostream>
+using namespace std;
+class Account {
+public:
+virtual void displayBalance() {
+cout << "Generic account balance.\n";
+}
+};
+class Savings : public Account {
+public:
+void displayBalance() {
+cout << "Savings Account Balance: ₹5000\n";
+}
+};
+class Current : public Account {
+public:
+void displayBalance() {
+cout << "Current Account Balance: ₹10000\n";
+}
+};
+int main() {
+Account *acc;
+Savings s;
+Current c;
+acc = &s; acc->displayBalance();
+acc = &c; acc->displayBalance();
+}
